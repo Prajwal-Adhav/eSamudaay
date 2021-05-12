cd838544efb03aa8e56191ad367ae89531d859b4
Prajwal Adhav <prajwaladhav21@gmail.com>
2021-05-12 15:43:01 +0530

    second commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,28 @@
+//  Ajax with jQuery
+// $.ajax({
+//     url : "https://run.mocky.io/v3/26ed4273-3dc5-44b1-b0c9-0ff0a576b344",
+//     type: "GET",
+//     dataType: "json",
+//     success: function(data){
+//         console.log(data);
+//     },
+//     error: function(){
+//         console.log("Error in the request");
+//     }  
+// });
+
+
+// Mock url=https://run.mocky.io/v3/26ed4273-3dc5-44b1-b0c9-0ff0a576b344
+// delete link=https://designer.mocky.io/manage/delete/26ed4273-3dc5-44b1-b0c9-0ff0a576b344/SX04eYMWjw613F4n9Fia58lvH5fexGbzsGar
+
+// var url="https://run.mocky.io/v3/26ed4273-3dc5-44b1-b0c9-0ff0a576b344";
+//         var xhr= new XMLHttpRequest();
+//         xhr.oneerror=function(){
+//         alert("Error");
+//         }
+//         xhr.onload=function(){
+//             alert(this.responseText);
+//         };
+//         xhr.open('Get',url,true);
+//         xhr.withCredentials=true;
+//         xhr.send();
